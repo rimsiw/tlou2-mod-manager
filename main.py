5b5a25d17b4fee3525065e77fbbd09f2996d70b1
rimsiw <rimwitkowska@proton.me>
2025-04-26 13:12:33 +0200

    PROFILES

--- main.py
+++ main.py
@@ -48,12 +48,23 @@ def question_setup():
             print("Exiting program as no path was set.")
             sys.exit()
 
-def add_mods(modsdir):
+def mod_data():
+    datadir = os.path.join(os.getcwd(), "data")
+    librarydir = os.path.join(datadir, "mods")
+    profilesdir = os.path.join(datadir, "profiles")
+
+    for d in [datadir, librarydir, profilesdir]:
+        if not os.path.exists(d):
+            os.makedirs(d)
+
+    return librarydir, profilesdir
+
+def add_mods(librarydir):
     """Add mods using text-based path input"""
     print("\n=== Add Mods ===")
 
     while True:
-        mod_path = questionary.path(message='Enter the full path to the mod file (.psarc) or directory containing mods.',).ask()
+        mod_path = questionary.path(message='Enter the full path to the mod file (.psarc) containing mods.').ask()
 
         mod_path = os.path.expanduser(mod_path)
         if not os.path.exists(mod_path):
@@ -68,11 +79,11 @@ def add_mods(modsdir):
                     continue
 
             file_name = os.path.basename(mod_path)
-            destination = os.path.join(modsdir, file_name)
+            destination = os.path.join(librarydir, file_name)
 
             try:
                 shutil.copy2(mod_path, destination)
-                print(f"Successfully added mod: {file_name}")
+                print(f"Successfully added mod '{file_name}' to the library.")
             except Exception as e:
                 print(f"Failed to add {file_name}: {str(e)}")
 
@@ -81,6 +92,87 @@ def add_mods(modsdir):
         add_more = questionary.confirm(message='Add more?')
         if add_more != True:
             break
+
+def save_profile(librarydir, profilesdir):
+    mods = [f for f in os.listdir(librarydir) if f.endswith('.psarc')]
+
+    if not mods:
+        print("No mods found in the library.")
+        return
+    
+    selections = questionary.checkbox("Select mods for this profile.", choices=mods).ask()
+    if not selections:
+        print("No mods were selected.")
+        return
+    
+    profilename = questionary.text("Please enter a name for this profile:").ask()
+    if not profilename:
+        print("Profile name cannot be empty.")
+        return
+
+    profilepath = os.path.join(profilesdir, f"{profilename}.json")
+    profiledata = {
+        "mods": selections
+    }
+
+    with open(profilepath, 'w', encoding='utf-8') as f:
+        json.dump(profiledata, f, indent=4)
+
+    print(f"Profile '{profilename}' has been saved")
+
+def load_profile(modsdir, profilesdir, librarydir):
+    profiles = [f[:-5] for f in os.listdir(profilesdir) if f.endswith('.json')]
+
+    if not profiles:
+        print("No profiles available at the current moment.")
+        return
+    
+    profileselected = questionary.select("Select a profile to load:", choices=profiles).ask()
+    if not profileselected:
+        return
+    
+    profilepath = os.path.join(profilesdir, f"{profileselected}.json")
+    with open(profilepath, 'r', encoding='utf-8') as f:
+        profile = json.load(f)
+    
+    enabled_future = profile.get("mods", [])
+
+    for f in os.listdir(modsdir):
+        fullpath = os.path.join(modsdir, f)
+        try:
+            if os.path.isfile(fullpath) or os.path.islink(fullpath):
+                os.remove(fullpath)
+            elif os.path.isdir(fullpath):
+                shutil.rmtree(fullpath)
+        except Exception as e:
+            print(f"Failed to delete {fullpath}: {e}")
+
+    for mod in enabled_future:
+        src = os.path.join(librarydir, mod)
+        dest = os.path.join(modsdir, mod)
+        if os.path.exists(src):
+            shutil.copy2(src, dest)
+            print(f"Activated: {mod}")
+        else:
+            print(f"Mod {mod} is missing in the library")
+
+    print(f"Selected profile '{profileselected} has been loaded")
+
+def delete_profile(profilesdir):
+    profiles = [f[:-5] for f in os.listdir(profilesdir) if f.endswith('.json')]
+
+    if not profiles:
+        print("No profiles to delete")
+        return
+
+    profileselected = questionary.select("Select a profile to delete:", choices=profiles).ask()
+
+    profilepath = os.path.join(profilesdir, f"{profileselected}.json")
+    try:
+        os.remove(profilepath)
+        print(f"Profile '{profileselected} has been deleted")
+    except Exception as e:
+        print(f"Failed to delete profile: {str(e)}")
 
 def main():
     opt = settings_load()
@@ -109,79 +201,35 @@ def main():
     if not os.path.exists(modsdir):
         os.makedirs(modsdir)
 
-    modsf = [f for f in os.listdir(modsdir) 
-    if os.path.isfile(os.path.join(modsdir, f)) and f.endswith(".psarc")]
+    librarydir, profilesdir = mod_data()
 
-    disabledf = [f for f in os.listdir(disableddir) 
-    if os.path.isfile(os.path.join(disableddir, f)) and f.endswith(".psarc")]
+    while True:
+        choice = questionary.select(
+            "What do you want to do?",
+            choices=[
+                'Change The Path',
+                'Add mods to the library',
+                'Create a profile',
+                'Load a profile',
+                'Delete a profile',
+                'Exit'
+            ]).ask()
 
-    choice = questionary.select(
-        "What do you want to do?",
-        choices=[
-            'Change The Path',
-            'Add mods',
-            'Enable/Disable mods',
-            'Exit'
-        ]).ask()
-
-    if choice == 'Change The Path':
-        question_setup()
-        main()
-        return
-    elif choice == 'Add mods':
-        add_mods(modsdir)
-        main()
-        return
-    elif choice == 'Enable/Disable mods':
-        modsall = modsf + disabledf
-
-        if not modsall:
-            print("No mods found. Please add some mods first.")
-            input("Press Enter to continue...")
+        if choice == 'Change The Path':
+            question_setup()
             main()
             return
-
-        choices = [
-            {
-                'name': f,
-                'checked': f in modsf
-            }
-            for f in modsall
-        ]
-
-        select = questionary.checkbox(
-            'Select mods to enable (checked = enabled):',
-            choices=choices
-        ).ask()
-
-        if select is None:
-            main()
-            return
-
-        for f in modsall:
-            enabled_now = f in modsf
-            enabled_future = f in select
-
-            if enabled_now and not enabled_future:
-                try:
-                    shutil.move(os.path.join(modsdir, f), os.path.join(disableddir, f))
-                    print(f"Mod '{f}' is now disabled.")
-                except Exception as e:
-                    print(f"Error disabling '{f}': {str(e)}")
-
-            elif not enabled_now and enabled_future:
-                try:
-                    shutil.move(os.path.join(disableddir, f), os.path.join(modsdir, f))
-                    print(f"Mod '{f}' is now enabled.")
-                except Exception as e:
-                    print(f"Error enabling '{f}': {str(e)}")
-
-        input("\nPress Enter to continue...")
-        main()
-        return
-    elif choice == 'Exit':
-        print("Exiting program. Goodbye!")
-        return
+        elif choice == 'Add mods to the library':
+            add_mods(librarydir)
+        elif choice == 'Create a profile':
+            save_profile(librarydir, profilesdir)
+        elif choice == 'Load a profile':
+            load_profile(modsdir, profilesdir, librarydir)
+        elif choice == 'Delete a profile':
+            delete_profile(profilesdir)
+        elif choice == 'Exit':
+            print("Exiting program. Goodbye!")
+            break
 
 if __name__ == "__main__":
     print("=== The Last of Us Part II Mod Manager ===")
